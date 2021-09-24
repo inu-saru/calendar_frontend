569e75d3e1a00028e555738a3539ba071293ed53
inu_saru <inu_saru_eagle@yahoo.co.jp>
2021-09-24 23:38:02 +0900

    Add: create Calendar

---
+++ server/src/components/Calendar.vue
@@ -0,0 +1,117 @@
+<template>
+  <div>
+    <div class="content">
+      <h2>カレンダー{{ currentDate }}</h2>
+      <div class="button-area">
+        <button @click="prevMonth" class="button">前の月</button>
+        <button @click="nextMonth" class="button">次の月</button>
+      </div>
+      <div class="calendar">
+        <div
+          class="calendar-weekly"
+          v-for="(week, index) in calendars"
+          :key="index"
+        >
+          <div
+            class="calendar-daily"
+            v-for="(day, index) in week"
+            :key="index"
+          >
+            <div class="calendar-day">
+              {{ day.date }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import moment from "moment";
+export default {
+  data() {
+    return {
+      currentDate: moment(),
+    };
+  },
+  methods: {
+    getCalendar() {
+      let startDate = this.getStartDate();
+      const endDate = this.getEndDate();
+      const weekNumber = Math.ceil(endDate.diff(startDate, "days") / 7);
+
+      let calendars = [];
+      for (let week = 0; week < weekNumber; week++) {
+        let weekRow = [];
+        for (let day = 0;  day < 7; day++) {
+          weekRow.push({
+            date: startDate.get("date"),
+          });
+          startDate.add(1, "days");
+        }
+        calendars.push(weekRow);
+      }
+      return calendars;
+    },
+    getStartDate() {
+      let date = moment(this.currentDate);
+      date.startOf("month");
+      const youbiNum = date.day();
+      return date.subtract(youbiNum, "days");
+    },
+    getEndDate() {
+      let date = moment(this.currentDate);
+      date.endOf("month");
+      const youbiNum = date.day();
+      return date.add(6 - youbiNum, "days");
+    },
+    nextMonth() {
+      this.currentDate = moment(this.currentDate).add(1, "month");
+    },
+    prevMonth() {
+      this.currentDate = moment(this.currentDate).subtract(1, "month");
+    },
+  },
+  mounted(){
+  },
+  computed: {
+    calendars() {
+      return this.getCalendar();
+    },
+  },
+}
+</script>
+
+<style>
+.content{
+  margin:2em auto;
+  width:900px;
+}
+.button-area{
+  margin:0.5em 0;
+}
+.button{
+  padding:4px 8px;
+  margin-right:8px;
+}
+.calendar{
+  max-width:900px;
+  border-top:1px solid #E0E0E0;
+  font-size:0.8em;
+}
+.calendar-weekly{
+  display:flex;
+  border-left:1px solid #E0E0E0;
+  /* background-color: black; */
+}
+.calendar-daily{
+  flex:1;
+  min-height:125px;
+  border-right:1px solid #E0E0E0;
+  border-bottom:1px solid #E0E0E0;
+  margin-right:-1px;
+}
+.calendar-day{
+  text-align: center;
+}
+</style>
