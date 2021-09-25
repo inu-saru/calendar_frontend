f847d75f58636887108f8594b39d78ea133c1610
inu_saru <inu_saru_eagle@yahoo.co.jp>
2021-09-25 19:29:55 +0900

    Add: イベントの表示

--- server/src/components/Calendar.vue
+++ server/src/components/Calendar.vue
@@ -24,7 +24,14 @@
             :key="index"
           >
             <div class="calendar-day">
-              {{ day.date }}
+              {{ day.day }}
+            </div>
+            <div v-for="dayEvent in day.dayEvents" :key="dayEvent.id" >
+              <div
+                class="calendar-event"
+                :style="`background-color:${dayEvent.color}`" >
+                {{ dayEvent.name }}
+              </div>
             </div>
           </div>
         </div>
@@ -37,7 +44,28 @@ import moment from 'moment'
 export default {
   data () {
     return {
-      currentDate: moment()
+      currentDate: moment(),
+      events: [
+        { id: 1, name: 'ミーティング', start: '2021-10-01', end: '2021-10-01', color: 'blue' },
+        { id: 2, name: 'イベント', start: '2021-10-02', end: '2021-10-03', color: 'limegreen' },
+        { id: 3, name: '会議', start: '2021-10-06', end: '2021-10-06', color: 'deepskyblue' },
+        { id: 4, name: '有給', start: '2021-10-08', end: '2021-10-08', color: 'dimgray' },
+        { id: 5, name: '海外旅行', start: '2021-10-08', end: '2021-10-11', color: 'navy' },
+        { id: 6, name: '誕生日', start: '2021-10-16', end: '2021-10-16', color: 'orange' },
+        { id: 7, name: 'イベント', start: '2021-10-12', end: '2021-10-15', color: 'limegreen' },
+        { id: 8, name: '出張', start: '2021-10-12', end: '2021-10-13', color: 'teal' },
+        { id: 9, name: '客先訪問', start: '2021-10-14', end: '2021-10-14', color: 'red' },
+        { id: 10, name: 'パーティ', start: '2021-10-15', end: '2021-10-15', color: 'royalblue' },
+        { id: 12, name: 'ミーティング', start: '2021-10-18', end: '2021-10-19', color: 'blue' },
+        { id: 13, name: 'イベント', start: '2021-10-21', end: '2021-10-21', color: 'limegreen' },
+        { id: 14, name: '有給', start: '2021-10-20', end: '2021-10-20', color: 'dimgray' },
+        { id: 15, name: 'イベント', start: '2021-10-25', end: '2021-10-28', color: 'limegreen' },
+        { id: 16, name: '会議', start: '2021-10-21', end: '2021-10-21', color: 'deepskyblue' },
+        { id: 17, name: '旅行', start: '2021-10-23', end: '2021-10-24', color: 'navy' },
+        { id: 18, name: 'ミーティング', start: '2021-10-28', end: '2021-10-28', color: 'blue' },
+        { id: 19, name: '会議', start: '2021-10-12', end: '2021-10-12', color: 'deepskyblue' },
+        { id: 20, name: '誕生日', start: '2021-10-30', end: '2021-10-30', color: 'orange' }
+      ]
     }
   },
   methods: {
@@ -52,15 +80,25 @@ export default {
       for (let week = 0; week < weekNumber; week++) {
         let weekRow = []
         for (let day = 0; day < 7; day++) {
+          let dayEvents = this.getDayEvents(calendarDate)
           weekRow.push({
-            date: calendarDate.get('date'),
-            month: calendarDate.format('YYYY-MM')
+            day: calendarDate.get('date'),
+            month: calendarDate.format('YYYY-MM'),
+            dayEvents
           })
           calendarDate.add(1, 'days')
         }
         calendars.push(weekRow)
       }
       return calendars
+    },
+    getDayEvents (date) {
+      return this.events.filter(event => {
+        let startDate = moment(event.start).format('YYYY-MM-DD')
+        let endDate = moment(event.end).format('YYYY-MM-DD')
+        let Date = date.format('YYYY-MM-DD')
+        if (startDate <= Date && endDate >= Date) return true
+      })
     },
     getStartDate () {
       let date = moment(this.currentDate)
@@ -138,6 +176,12 @@ export default {
   border-right:1px solid #E0E0E0;
   margin-right:-1px;
   text-align:center;
+}
+.calendar-event{
+  color:white;
+  margin-bottom:1px;
+  height:25px;
+  line-height:25px;
 }
 .outside{
   background-color: #f7f7f7;
